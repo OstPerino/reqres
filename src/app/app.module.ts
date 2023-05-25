6a90dd2f4b9229312c94215b349dd9ad0d585b96
Nikita <kucenko.nikita@gmail.com>
2023-05-26 01:08:25 +0700

    Test angular cli by adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from '../components/users-list/users-list.component';
+import { ResourcesListComponent } from '../components/resources-list/resources-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    ResourcesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/components/resources-list/resources-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resources-list',
+  templateUrl: './resources-list.component.html',
+  styleUrls: ['./resources-list.component.scss']
+})
+export class ResourcesListComponent {
+
+}
